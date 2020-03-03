ad342b8418f7a07a8285d830cf28a3cea5a251b5
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-03 09:51:26 +0200

    added error handling - error message BRAKES app

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import { Mutation } from 'react-apollo'
 import { SIGNUP_USER } from '../../queries'
+import Error from "../Error";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirmation: ""
+}
 
 class Signup extends React.Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirmation: ""
-  }
+  state = { ...initialState }
 
   handleChange = (event) => {
     const { name, value } = event.target
     this.setState({ [name]: value })
   }
 
+  clearState = () => {
+    this.setState({ ...initialState })
+  }
+
   handleSubmit = (event, signupUser) => {
     event.preventDefault();
     signupUser().then(data => {
-      console.log(data);
+      console.log(data)
+      this.clearState()
     })
+  }
+
+  validateForm = () => {
+    const { username, email, password, passwordConfirmation } = this.state
+
+    const isInvalid = !username || !email || !password || password !== passwordConfirmation
+    return isInvalid
   }
 
   render() {
@@ -39,8 +54,8 @@ class Signup extends React.Component {
                 <input type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange} />
                 <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
                 <input type="password" name="passwordConfirmation" placeholder="Confirm password" value={passwordConfirmation} onChange={this.handleChange} />
-
-                <button type="submit" className="button-primary">Submit</button>
+                <button type="submit" disabled={loading || this.validateForm()} className="button-primary">Submit</button>
+                {error && <Error error={error} />}
               </form>
             )
           }}
