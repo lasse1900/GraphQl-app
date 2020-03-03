5f8022f6d8b6a3e3edca22f6d9c571da8cf3ed06
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-03 15:32:36 +0200

    added signin mutation to client

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
+import { SIGNIN_USER } from '../../queries'
+import Error from "../Error";
 
+const initialState = {
+  username: "",
+  password: "",
+}
 
 class Signin extends React.Component {
+  state = { ...initialState }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  clearState = () => {
+    this.setState({ ...initialState })
+  }
+
+  handleSubmit = (event, signinUser) => {
+    event.preventDefault();
+    signinUser().then(data => {
+      console.log(data)
+      this.clearState()
+    })
+  }
+
+  validateForm = () => {
+    const { username, password } = this.state
+
+    const isInvalid = !username || !password
+    return isInvalid
+  }
+
   render() {
+    const { username, password } = this.state
     return (
-      <div>Signin</div>
+      <div className="App">
+        <h2 className="App">Signin</h2>
+        <Mutation
+          mutation={SIGNIN_USER}
+          variables={{ username, password }}
+        >
+          {(signinUser, { data, loading, error }) => {
+            return (
+              <form className="form"
+                onSubmit={event => this.handleSubmit(event, signinUser)}>
+                <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange} />
+                <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
+                <button type="submit" disabled={loading || this.validateForm()} className="button-primary">Submit</button>
+                {error && <Error error={error} />}
+              </form>
+            )
+          }}
+        </Mutation>
+      </div>
     )
   }
 }
